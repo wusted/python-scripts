000438c66d88c69a1c0aef0be961c454624adb53
Jean Pereira <jeankpayales444@gmail.com>
2021-06-25 22:57:37 -0600

    update oop_sample

--- oop_sample.py
+++ oop_sample.py
@@ -16,6 +16,9 @@ class Point:
     def distanceFromOrigin(self):
         return ((self.x ** 2) + (self.y **2)) ** 0.5
 
+    def __str__(self):
+        return "x=" + str(self.x) + ", y=" + str(self.y)
+
 
 def distance(point1, point2):
     xdiff = point2.getx() - point1.getx()
@@ -30,3 +33,5 @@ print(z.distanceFromOrigin())
 p = Point(4, 3)
 q = Point(0, 0)
 print(distance(p, q))
+
+print(z)
