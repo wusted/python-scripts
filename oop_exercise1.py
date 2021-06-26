5384ded799a2fcbb979613e15cd25e3a3478c1f8
Jean Pereira <jeankpayales444@gmail.com>
2021-06-26 16:19:35 -0600

    point objetcs distance from other point object

---
+++ oop_exercise1.py
@@ -0,0 +1,31 @@
+import math
+
+class Point:
+    """ Point class for representing and manipulating x,y coordinates. """
+
+    def __init__(self, initX, initY):
+        """ Create a new point at the given coordinates. """
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2) ** 0.5)
+
+    def distanceFromPoint(self, otherP):
+        dx = (otherP.getX() - self.x)
+        dy = (otherP.getY() - self.y)
+        return math.sqrt(dy**2 + dx**2)
+
+p = Point(3, 3)
+q = Point(6, 7)
+
+print(p.distanceFromPoint(q))
+
+
+
